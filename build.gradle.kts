3da2a871aa21b7ed6540f11e296572b550757140
tsubasa-fukuchi <tsubasa.fukuchi@linecorp.com>
2020-02-06 17:36:18 +0900

    Apply .kts to .gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.50'
-    ext.grpc_version = "1.3.0"
+    val kotlinVersion by extra("1.3.50")
     repositories {
         google()
         jcenter()
         
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.3'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath "com.google.protobuf:protobuf-gradle-plugin:0.8.8"
+        classpath("com.android.tools.build:gradle:3.5.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.8")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -25,8 +24,8 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.create("clean", type = Delete::class) {
+    delete(rootProject.buildDir)
 }
 
 
